f4ff688ed9def71de6f34cce1b812ac36352c978
yashmane987 <yashmane4045@gmail.com>
2025-09-13 16:56:05 +0530

    done with spread operater

--- Nodejs + typescript/1_project/index.ts
+++ Nodejs + typescript/1_project/index.ts
@@ -1,1 +1,56 @@
-console.log("hello")
+
+
+// Tempalte Literals
+
+const username : string = 'Saurabh mane'
+console.log(`my name is ${username}`)
+
+function myfunction (name="Yash"){
+    console.log(`My name is ${name}`)
+} 
+
+myfunction();
+
+
+
+// spread operator  array and objects 
+
+// ex with array 
+const arr :number[] = [10,22,34,45];
+const arr1 :number[] =[...arr,55,67,88]
+console.log(arr1)
+
+
+
+
+
+// with the help of interface we can declare datatype of object in array
+
+interface userdetailsinterface  {
+    name :string,
+    std:number;
+}
+
+
+// ex with object
+
+const userdata : userdetailsinterface = {
+    name : "yash",
+    std:11
+}
+
+const anotheruser ={
+    name1:"ketan",
+    std1:"9th"
+}
+
+const alluser ={
+    ...userdata,
+    ...anotheruser
+}
+console.log(alluser)
+
+
+// Rest Opearter
+
+
