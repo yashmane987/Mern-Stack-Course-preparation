b762932054b2c415acdbe809388f076d5ca22554
yashmane987 <yashmane4045@gmail.com>
2025-09-16 17:12:29 +0530

    done with todays

---
+++ 4 month september/Javascript/0_Temporal Dead Zone.js
@@ -0,0 +1,10 @@
+console.log(a); 
+let a = 10;
+
+{
+  console.log(x); 
+  const x = 5;
+}
+
+console.log(b); 
+var b = 20;
--- 4 month september/Javascript/5_Syncronus and Asyncronus.js
+++ 4 month september/Javascript/5_Syncronus and Asyncronus.js
@@ -1,47 +1,61 @@
 // Synchronus - Javscript code run line by line
 
-console.log('hello javScript');
-console.log('1')
-console.log('2')
-console.log('3')
-
+console.log("hello javScript");
+console.log("1");
+console.log("2");
+console.log("3");
 
 // Asynchronus means run code line by line but wait for some time to execute code of specific second then it will run at last in output
 
-console.log('1');
-setTimeout(()=>{    //Asynchronus operation
-console.log('Run in last ')
-},[1000])
-console.log('2')
+console.log("1");
+setTimeout(() => {
+  //Asynchronus operation
+  console.log("Run in last ");
+}, [1000]);
+console.log("2");
 
-
-// 1- Promises  :  - 
+// 1- Promises  :  -
 // 2 - async and await :  latest version of promises, it is ascyncronus operation , if we want to to do any db call we can use async and await
 // 3 - callback  :   is a function can pass an  argument from one function to another function
 
-
-
 //Callback ex
 
-function myfunction(callback){
-     callback()
-    console.log('Yash');
-   
+function myfunction(callback) {
+  callback();
+  console.log("Yash");
 }
 
-function secondfunction(){
-    console.log('second function callback')
+function secondfunction() {
+  console.log("second function callback");
 }
 myfunction(secondfunction);
 
-
-
 // async and await
 
-const getdata = async  () =>{
-    try {
-        const Userdata =  await User.find()
-    } catch (error) {
-        console.log(error)
+const getdata = async () => {
+  try {
+    const Userdata = await User.find();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//promises ex - having 3stages 1 - resolve ,reject,pending
+
+let success = true;
+
+const pro = new Promise((resolve, reject) => {
+  try {
+     
+    if (success == false) resolve("resolve work");
+    else {
+      reject("reject work");
     }
-}
+  } catch (error) {
+    console.log(error);
+  }
+});
+pro.then((e) => console.log(e)).catch((e) => console.log(e));
+
+
+
